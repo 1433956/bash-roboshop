0b685f50327f9beb3543724dc4b8359b3bf93d5e
raghu <raghumurukutla96@gmail.com>
2026-04-22 22:21:48 +0530

    adding catalogue script

---
+++ catalogue.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+ Start_time=$(date +%s)
+ 
+LOG_FOLDER="/var/log/roboshop-logs"
+mkdir -p $LOG_FOLDER
+LOG_FILENAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$LOG_FILENAME.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+USERID=$(id -u)
+
+#check user is logged as root user
+
+if [ $? -ne 0 ]
+then
+   echo -e "$R please log user as root user::  $W" &>> $LOG_FILE
+   exit 1
+else 
+    echo -e "$G logged as a root user:: $W " &>> $LOG_FILE
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then 
+       echo -e "$R $2 is Failure:: $W" &>> $LOG_FILE
+       exit 1
+   else
+      echo -e "$G $2 is Success:: $W" &>> $LOG_FILE
+   fi
+
+}
+
+dnf module disble nodejs -y
+
+VALIDATE $? "disabling the nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enable  the nodejs version of 20"
+
+dnf list installed nodejs
+if [ $? -ne 0 ]
+then 
+   dnf install nodejs -y
+   echo -e "$G not installed in machine installing nodejs $W" &>> $LOG_FILE
+else
+   echo -e "$Y installed in machine Skipping installing nodejs $W" &>> $LOG_FILE
+   exit 1
+fi
+   
+    
+VALIDATE $? "installing  nodejs"
+
+
+
