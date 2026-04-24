ce0ee0cf839984ef03f7a1c3807c3cf109b24f1d
raghu <raghumurukutla96@gmail.com>
2026-04-24 11:28:04 +0530

    chnages to mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -71,16 +71,61 @@ fi
 
 mkdir -p /app
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "creating directory for app"
+
+rm -rf /app/*
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
  VALIDATE $? "downloading the code "
 
 cd /app 
+ unzip /tmp/catalogue.zip  &>>$LOG_FILE
+ VALIDATE $? "unziping the app folder"
 
- unzip /tmp/catalogue.zip
+ npm install &>> $LOG_FILE
 
- cd $current_directory/app
+ VALIDATE $? "npm installing::"
+
+
+cp $current_directory /catalogue.service /etc/systemd/system/catalogue.service &>> $LOG_FILE
+ VALIDATE $? "copying  catalogue service" 
+
+ systemctl daemon-reload &>> $LOG_FILE
+
+VALIDATE $? "daemon reload " 
+
+systemctl enable catalogue &>> $LOG_FILE
+VALIDATE $? "enable catalogue "
+systemctl start catalogue &>> $LOG_FILE
+
+VALIDATE $? "start catalogue "
  
-echo "$current_directory"
+ cp $current_directory/mongo.repo  /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
+
+ VALIDATE $? "copy mongo repo "
+
+ dnf install mongodb-mongosh -y &>>$LOG_FILE
+
+ VALIDATE $? "installing mongodb client "
+
+ DBStatus=$(mongosh --host mongodb.devops26.sbs --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $DBStatus -lt 0 ]
+then
+    echo -e "$G data is not load, going to load data $W" | tee -a $LOG_FILE
+
+    mongosh --host mongodb.devops26.sbs </app/db/master-data.js &>>$LOG_FILE
+else 
+     echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
+End_time=$(date +%s)
+
+Total_execution_time= $($(Start_time - $End_time))
+
+echo -e " $G Total_execution_time:: $Total_execution_time:: $W"
+ 
+
+
 
 
 
