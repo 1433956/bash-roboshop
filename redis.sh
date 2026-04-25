a053823ac09c0ce6d7ef724acd2e8d9c7c15e174
raghu <raghumurukutla96@gmail.com>
2026-04-25 16:05:39 +0530

    adding changes to redis

--- redis.sh
+++ redis.sh
@@ -52,7 +52,7 @@ else
 fi
 VALIDATE $? "install  redis version of 7 "
 
-sed -i -e '/s/127.0.0.1/0.0.0.0/g -e /protected-mode/c protected-mode no' /etc/redis/redis.conf
+sed -i -e '/s/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf
 
 VALIDATE $? "redis conf change"
 
