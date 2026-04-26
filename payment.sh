f374b906ffe84723c9bbe5e15f02f380ba63e5cd
raghu <raghumurukutla96@gmail.com>
2026-04-26 22:11:21 +0530

    editing payment script

--- payment.sh
+++ payment.sh
@@ -1,104 +1,78 @@
 #!/bin/bash
- Start_time=$(date +%s)
- 
-LOG_FOLDER="/var/log/roboshop-logs"
-mkdir -p $LOG_FOLDER
-LOG_FILENAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$LOG_FILENAME.log"
 
+START_TIME=$(date +%s)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-W="\e[0m"
-current_directory=$PWD
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
-USERID=$(id -u)
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-#check user is logged as root user
-
-if [ $? -ne 0 ]
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
 then
-   echo -e "$R please log user as root user::  $W" &>> $LOG_FILE
-   exit 1
-else 
-    echo -e "$G logged as a root user:: $W " &>> $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then 
-       echo -e "$R $2 is Failure:: $W" | tee -a $LOG_FILE
-       exit 1
-   else
-      echo -e "$G $2 is Success:: $W" | tee -a $LOG_FILE
-   fi
-
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "Install Python3 packages"
 
-dnf list --installed python3 gcc python3-devel &>> $LOG_FILE
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
-then 
-   dnf install python3 gcc python3-devel -y &>> $LOG_FILE
-   echo -e "$G not installed in machine installing python3 $W" &>> $LOG_FILE
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
 else
-   echo -e "$Y installed in machine Skipping installing python3 $W" &>> $LOG_FILE
-   
-   
-fi
-     
-VALIDATE $? "installing  python3"
-
-#create system user 
-
-user=$(id roboshop)
-
-if [ $? -eq 0 ]
-then 
-   echo -e "$Y system user is created skiping user creation::$user $W" | tee -a $LOG_FILE
-   
-else
-   echo -e "$G system user not created, Creting system user:: $user $W" | tee -a $LOG_FILE
-   
-   useradd --system --home /app --shell /sbin/nologin  --comment "creating system user" roboshop
-   
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app &>>$LOG_FILE
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-VALIDATE $? "creating directory for app"
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading payment"
 
-
-
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$LOG_FILE
- VALIDATE $? "downloading the code "
-
+rm -rf /app/*
 cd /app 
-rm -rf /app/* &>>$LOG_FILE
-VALIDATE $? "removing app folder"
-unzip /tmp/payment.zip  &>>$LOG_FILE
-VALIDATE $? "unziping the app folder"
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "unzipping payment"
 
 pip3 install -r requirements.txt &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
 
-VALIDATE $? "download the dependencies"
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+VALIDATE $? "Copying payment service"
 
-cp $current_directory/payment.service /etc/systemd/system/payment.service &>> $LOG_FILE
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon Reload"
 
-VALIDATE $? "copying  payment service" 
+systemctl enable payment &>>$LOG_FILE
+VALIDATE $? "Enable payment"
 
- systemctl daemon-reload &>> $LOG_FILE
+systemctl start payment &>>$LOG_FILE
+VALIDATE $? "Starting payment"
 
-VALIDATE $? "daemon reload " 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-systemctl enable payment &>> $LOG_FILE
-VALIDATE $? "enable payment "
-systemctl start payment &>> $LOG_FILE
-
-End_time=$(date +%s)
-
-Total_execution_time=$(($End_time - $Start_time))
-
-echo -e " $G Total_execution_time:: $Total_execution_time:: $W"
- 
-
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
